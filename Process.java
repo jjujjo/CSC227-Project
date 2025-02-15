386bb43abea40e18fa3353a1e148fe53ca4581a7
Lobaali <lobaali@gmail.com>
2025-02-15 23:39:28 +0300

    .

--- Process.java
+++ Process.java
@@ -1,16 +1,18 @@
 public class Process{
     private int processID;
     private int burstTime;
-    private int turnaroundTime;
+    private double turnaroundTime;
     private int arrivalTime;
-    private int waitingTime;
-    private int executionTime;
+    private double waitingTime; //I changed to double 
+    private int remainingTime;
+    private int completionTime;
 
     public Process(int processID,  int arrivalTime, int burstTime)
     {
         this.processID=processID;
         this.arrivalTime=arrivalTime;
         this.burstTime=burstTime;
+        this.remainingTime=burstTime;
     }
 
     public int getProcessID()
@@ -18,12 +20,17 @@ public class Process{
         return processID;
     }
 
+    public int getCompletionTime()
+    {
+        return completionTime;
+    }
+
     public int getBurstTime()
     {
         return burstTime;
     }
 
-    public int getTurnaroundTime()
+    public double getTurnaroundTime()
     {
         return turnaroundTime;
     }
@@ -33,9 +40,14 @@ public class Process{
         return arrivalTime;
     }
 
-    public int getWaitingTime()
+    public double getWaitingTime()
     {
         return waitingTime;
+    }
+
+    public int getRemainingTime()
+    {
+        return remainingTime;
     }
 
     public void setBurstTime(int burstTime)
@@ -43,13 +55,24 @@ public class Process{
         this.burstTime=burstTime;
     }
 
-    public void setWaitingTime(int waitingTime)
+    public void setWaitingTime(double waitingTime)
     {
         this.waitingTime=waitingTime;
     }
 
-    public void updateProcess()
+    public void setRemainingTime(int remainingTime)
     {
-        //update attributes 
+        this.remainingTime=remainingTime;
     }
-}
+
+    public void setCompletionTime(int completionTime)
+    {
+        this.completionTime=completionTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime)
+    {
+        this.turnaroundTime=turnaroundTime;
+    }
+
+ }
---
+++ ProcessScheduling.java
@@ -0,0 +1,59 @@
+import java.util.List;
+
+public class ProcessScheduling {
+
+    public static void simulateSRTF(List<Process> processes) {
+        int currentTime = 0, completed = 0, contextSwitchTime = 1;
+        int totalWaitingTime = 0, totalTurnaroundTime = 0;
+        Process currentProcess = null; //initial value 
+
+        while (completed < processes.size()) { //still we are not done with all processes 
+            Process shortest = null; //initial value we will change later when we find the shortest process
+            int minRemainingTime = Integer.MAX_VALUE; //Integer.MAX_VALUE is the largest value for an integer accepted by java -> we do this because we are searching for min, when we compare we want this to be for sure bigger
+
+            // The goal here is to find the process with the shortest remaining time (burst time) 
+            for (Process p : processes) {
+                if (p.getArrivalTime() <= currentTime && p.getRemainingTime() > 0) { 
+                    if (p.getRemainingTime() < minRemainingTime || 
+                        (p.getRemainingTime() == minRemainingTime && p.getArrivalTime() < (shortest != null ? shortest.getArrivalTime() : Integer.MAX_VALUE))) {
+                        /* we have two cases to enter the if:
+                            1- Remaining time is less than the min -> we will enter so it can become the min
+                            2- Remaining time equals the min and arrival time has two conditions:
+                                2a- If the shortest is null (first time we enter) then we will compare that the arrival time is less than the Integer.MAX_VALUE (which is always true)
+                                2b- If the shortest time is not null then we must then we will compare the arrival times since both burst times are equal -> apply FCFS
+                         */
+                        shortest = p;
+                        minRemainingTime = p.getRemainingTime(); //update the min remaining time so we use it in the comparision the next time we enter the loop
+                    }
+                }
+            }
+
+            if (shortest == null) {
+                // No process is ready, move time forward
+                currentTime++;
+                continue;
+            }
+
+            if (currentProcess != null && currentProcess != shortest) { //we have a process that is shorter than the process we are working on -> context switch
+                currentTime += contextSwitchTime;
+            }
+
+            // Process execution
+            shortest.setRemainingTime(shortest.getRemainingTime()-1);
+            currentTime++;
+
+            if (shortest.getRemainingTime() == 0) {
+                // Process finished
+                completed++;
+                shortest.setCompletionTime(currentTime);
+                shortest.setTurnaroundTime(shortest.getCompletionTime() - shortest.getArrivalTime());
+                shortest.setWaitingTime(shortest.getTurnaroundTime() - shortest.getBurstTime()); //this is why I changes waiting time to double !!!!
+                
+                totalTurnaroundTime += shortest.getTurnaroundTime();
+                totalWaitingTime += shortest.getWaitingTime();
+            }
+
+            currentProcess = shortest;
+        }
+    }
+}
