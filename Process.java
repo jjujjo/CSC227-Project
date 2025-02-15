d47cefdcd41481c45c2eb6d30e34dc39ff0d6fde
Lobaali <161109796+Lobaali@users.noreply.github.com>
2025-02-15 21:56:14 +0300

    Add files via upload

---
+++ Process.java
@@ -0,0 +1,54 @@
+public class Process{
+    private int processID;
+    private int burstTime;
+    private int turnaroundTime;
+    private int arrivalTime;
+    private int waitingTime;
+
+    public Process(int processID,  int arrivalTime, int burstTime)
+    {
+        this.processID=processID;
+        this.arrivalTime=arrivalTime;
+        this.burstTime=burstTime;
+    }
+
+    public int getProcessID()
+    {
+        return processID;
+    }
+
+    public int getBurstTime()
+    {
+        return burstTime;
+    }
+
+    public int getTurnaroundTime()
+    {
+        return turnaroundTime;
+    }
+
+    public int getArrivalTime()
+    {
+        return arrivalTime;
+    }
+
+    public int getWaitingTime()
+    {
+        return waitingTime;
+    }
+
+    public void setBurstTime(int burstTime)
+    {
+        this.burstTime=burstTime;
+    }
+
+    public void setWaitingTime(int waitingTime)
+    {
+        this.waitingTime=waitingTime;
+    }
+
+    public void updateProcess()
+    {
+        //update attributes 
+    }
+}
